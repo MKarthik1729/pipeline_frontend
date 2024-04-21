99fa62fd5030015d1d6cb8926ce3d23b2a214bf4
Mkarthik1729 <karthik.m20@iiits.in>
2024-04-21 16:32:55 +0530

    background change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <div className='set'>
 
       <p className='para'>The project is to predict whether a startup which is currently Operating, IPO, Acquired, or closed. <br /><br />
-      The AdaBoost Classifier and Gradient Boost algorithms are used in this project
+      The AdaBoost Classifier and Gradient Boost algorithms are used in this project <br /><br />
+      {/* Our model accuracy :<br /> */}
+      Train Accuracy  : 0.9845143155136669<br />
+      Test Accuracy  : 0.9849660400396836
       </p>
       <FormData />
       </div>
